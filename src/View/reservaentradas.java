b290cbb46f9853ca0a3233ca6bfe075c2eba8920
kepa <k_izaguirrefer@fpzornotza.com>
2025-05-13 13:28:45 +0200

    segundo commit

--- src/View/reservaentradas.java
+++ src/View/reservaentradas.java
@@ -81,7 +81,7 @@ public class reservaentradas extends JFrame {
 		model.addColumn("NAN");
 		model.addColumn("Ordaintze Metodoa");
 		model.addRow(new Object[]{"1", "Kepa", "Capipe", "12345678P", "Paypal"});
-		model.addRow(new Object[]{"2", "Victor", "Garralon", "32414425G", "Visa"});
+		model.addRow(new Object[]{"2", "Unai", "Garralon", "32414425G", "Visa"});
 		model.addRow(new Object[]{"3", "Sergio", "Calvinho", "89204294K", "Visa"});
 		model.addRow(new Object[]{"4", "JeanCarlo", "Toro", "323486597", "Mastercard"});
 		/**/
