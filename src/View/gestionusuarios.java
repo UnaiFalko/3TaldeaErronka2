0ebac42f0cfce7106dd7fc5b3436c78f20cd5a0e
Ekain <e_monteceloani@fpzornotza.com>
2025-05-15 09:28:38 +0200

    view javadoc autorea eta bertsiñoa

--- src/View/gestionusuarios.java
+++ src/View/gestionusuarios.java
@@ -12,6 +12,11 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/*
+ * @author EkainKepaUnai
+ * @version 1.0
+ */
+
 public class gestionusuarios extends JFrame {
 
 	private static final long serialVersionUID = 1L;
--- src/View/reservaentradas.java
+++ src/View/reservaentradas.java
@@ -14,6 +14,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 
+/*
+ * @author EkainKepaUnai
+ * @version 1.0
+ */
+
 public class reservaentradas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
